a29c0ba6ff54c81d087f8397a9dbec9e3196db88
tracer-ics2020 <tracer@njuics.org>
2023-02-12 06:20:59 -0800

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 28f1f2aea3df4825a5f33adc41170699            Boot ID: 1d4551091a18473fae5ea476ff463caa     Virtualization: vmware   Operating System: Ubuntu 20.04.5 LTS             Kernel: Linux 5.4.0-138-generic       Architecture: x86-64  06:20:59 up  2:30,  1 user,  load average: 0.01, 0.06, 0.02

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -42,11 +42,80 @@ void reg_test() {
 }
 
 void isa_reg_display() {
-  for(int i = 0 ; i < 8 ; i++){
-    printf("%s\t %x\n" , regsl[i] , cpu.gpr[i]._32);
-  }
+  // for(int i = 0 ; i < 8 ; i++){
+  //   printf("%s\t %x\n" , regsl[i] , cpu.gpr[i]._32);
+  // }
+
+  printf("%%eax: 0x%-12x",cpu.eax);
+  printf("%%ax: 0x%-12x",cpu.eax & 0xffff);
+  printf("%%ah: 0x%-12x",(cpu.eax >> 8) & 0xff);
+  printf("%%al: 0x%-12x\n",cpu.eax & 0xff);
+  
+  printf("%%ebx: 0x%-12x",cpu.ebx);
+  printf("%%bx: 0x%-12x",cpu.ebx & 0xffff);
+  printf("%%bh: 0x%-12x",(cpu.ebx >> 8) & 0xff);
+  printf("%%bl: 0x%-12x\n",cpu.ebx & 0xff);
+  
+  printf("%%ecx: 0x%-12x",cpu.ecx);
+  printf("%%cx: 0x%-12x",cpu.ecx & 0xffff);
+  printf("%%ch: 0x%-12x",(cpu.ecx >> 8) & 0xff);
+  printf("%%cl: 0x%-12x\n",cpu.ecx & 0xff);
+  
+  printf("%%edx: 0x%-12x",cpu.edx);
+  printf("%%dx: 0x%-12x",cpu.edx & 0xffff);
+  printf("%%dh: 0x%-12x",(cpu.edx >> 8) & 0xff);
+  printf("%%dl: 0x%-12x\n",cpu.edx & 0xff);
+  
+  printf("%%ebp: 0x%-12x",cpu.ebp);
+  printf("%%bp: 0x%-12x",cpu.ebp & 0xffff);
+  printf("%%bph: 0x%-11x",(cpu.ebp >> 8) & 0xff);
+  printf("%%bpl: 0x%-11x\n",cpu.ebp & 0xff);
+  
+  printf("%%esi: 0x%-12x",cpu.esi);
+  printf("%%si: 0x%-12x",cpu.esi & 0xffff);
+  printf("%%sih: 0x%-11x",(cpu.esi >> 8) & 0xff);
+  printf("%%sil: 0x%-11x\n",cpu.esi & 0xff);
+  
+  printf("%%edi: 0x%-12x",cpu.edi);
+  printf("%%di: 0x%-12x",cpu.edi & 0xffff);
+  printf("%%dih: 0x%-11x",(cpu.edi >> 8) & 0xff);
+  printf("%%dil: 0x%-11x\n",cpu.edi & 0xff);
+  
+  printf("%%esp: 0x%-12x",cpu.esp);
+  printf("%%sp: 0x%-12x",cpu.esp & 0xffff);
+  printf("%%sph: 0x%-11x",(cpu.esp >> 8) & 0xff);
+  printf("%%spl: 0x%-11x\n",cpu.esp & 0xff);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+  char *pc ="pc";
+  if (strcmp(s,pc) == 0)
+    return cpu.pc;
+  int which_set = -1;
+  int reg_index = 0;
+  uint32_t reg_array[] = {cpu.eax, cpu.ecx, cpu.edx, cpu.ebx, cpu.esp, cpu.ebp, cpu.esi, cpu.edi};
+  for (int i = 0; i < 8; i++){
+    if (strcmp(s,regsl[i]) == 0){
+      which_set = 1;
+      reg_index = i;
+      break;
+    }
+    if (strcmp(s,regsw[i]) == 0){
+      which_set = 2;
+      reg_index = i;
+      break;
+    }
+    if (strcmp(s,regsb[i]) == 0){
+      which_set = 3;
+      reg_index = i;
+      break;
+    }
+  }
+  assert(!(which_set == -1));
+  switch (which_set){
+    case 1: return reg_array[reg_index];
+    case 2: return reg_array[reg_index] & 0xffff;
+    case 3: return reg_array[reg_index] & 0xff;
+  }
+  return -1;
 }
